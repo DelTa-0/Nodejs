08fec2531887f990d766a3119c59654b1121290f
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-01 15:24:47 +0545

    dynamic routing

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express=require("express")
-
+const express=require("express");
+const path=require("path");
 const app=express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
 app.get("/",function(req,res){
-    res.send("hello");
+    res.render("index");
 })
+
+app.get("/profile/:username",function(req,res){
+    
+    res.send(`welcome, ,${req.params.username}`);
+})
+
+app.get("/profile/:username/:age",function(req,res){
+    
+    res.send(`welcome, ,${req.params.username} you are ${req.params.age} years old`);
+})
+
 
 
 app.listen(3000);
